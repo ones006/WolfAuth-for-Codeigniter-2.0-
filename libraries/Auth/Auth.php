af09409c083f52520c42d903d20f9313206eb90b
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-02-16 10:06:01 +1000

    Created a simpleauth driver which will be our base auth driver.

--- libraries/Auth/Auth.php
+++ libraries/Auth/Auth.php
@@ -1,127 +1,128 @@
 <?php
 
 class Auth extends CI_Driver_Library {
-    
+
     /**
     * Configuration options for auth
-    * 
+    *
     * @var mixed
     */
     protected $_config = array(
-        
+
         'method'          => 'username',
         'password_length' => 8,
         'guest_role'      => 'Guest',
         'default_role'    => 'User'
-        
+
     );
-    
+
     /**
     * Define the user roles
-    * 
+    *
     * @var mixed
     */
     protected $_roles = array(
-        
+
         'Guest' => array(
             'role_id' => 0,
             'role_description' => 'A non logged in user'
-        ),        
+        ),
         'User' => array(
             'role_id' => 1,
             'role_description' => 'Just a standard user'
-        ),        
+        ),
         'Admin' => array(
             'role_id' => 10,
             'role_description' => 'A super administrator'
         ),
-        
+
     );
-    
+
     /**
     * Info about the current session / user
-    * 
+    *
     * @var mixed
     */
     protected $_auth_info = array(
-        
+
         'username',
         'user_id',
         'role_id',
-        
+
     );
-    
+
     /**
     * Codeigniter instance
-    * 
+    *
     * @var mixed
     */
     protected $CI;
-    
+
     /**
     * Constructor
-    * 
+    *
     */
     public function __construct()
     {
         $this->CI =& get_instance();
-        
+
         $this->valid_drivers[] = 'auth_facebook';
         $this->valid_drivers[] = 'auth_twitter';
+        $this->valid_drivers[] = 'auth_simpleauth';
     }
-    
+
     /**
     * Logs in a user
-    * 
+    *
     */
     public function login()
     {
-        
+
     }
-    
+
     /**
     * Logs out the current user
-    * 
+    *
     */
     public function logout()
     {
-        
+
     }
-    
+
     /**
     * Add a new user to the database
-    * 
+    *
     */
     public function add_user()
     {
-        
+
     }
-    
+
     /**
     * Edit a user
-    * 
+    *
     */
     public function edit_user()
     {
-        
+
     }
-    
+
     /**
     * Delete a user
-    * 
+    *
     */
     public function delete_user()
     {
-        
+
     }
-    
+
     /**
     * Get information about the current user
-    * 
+    *
     */
     public function get_this_user()
     {
-        
+
     }
-    
+
 }
---
+++ libraries/Auth/drivers/Auth_simpleauth.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* @name Facebook Driver
+* @category Driver
+* @package WolfAuth
+* @author Dwayne Charrington
+* @copyright 2011
+* @link http://ilikekillnerds.com
+*/
+
+class Auth_simpleauth extends CI_Driver {
+
+    public function index()
+    {
+         echo "Hello Simpleauth!";
+    }
+
+ }
